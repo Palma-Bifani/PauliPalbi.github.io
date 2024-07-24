f47f46a7c7fd008daea59c6819bd97633baf331f
Palma-Bifani <ppalma@das.uchile.cl>
2024-07-24 10:08:35 +0200

    need to update

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = $_POST['message'];
 $to = 'paulina.palma-bifani@oca.eu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Web email from:  $name";
 $email_body = "New message from your website.\n\n"."Details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@palma-bifani.github.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: \n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
