76452aea289db30bce532fa66c303d8b94008bb0
Palma-Bifani <ppalma@das.uchile.cl>
2024-07-24 10:02:11 +0200

    contact update

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Palma-Bifani.github.io
 
 
-Webpage with personal information of me, Paulina Palma-Bifani
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,10 +14,10 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'ppalma@das.uchile.cl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'paulina.palma-bifani@oca.eu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Web email from:  $name";
+$email_body = "New message from your website.\n\n"."Details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@palma-bifani.github.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
